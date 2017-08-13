bc78503eddbeb9d8fee69a7469d5f42773478ce8
Marco Grier <mlgrier@gmail.com>
2017-08-12 19:15:41 -0500

    update

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -44,6 +44,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return currencyArray[row]
     }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
+    }
+    
     
     
 //    
